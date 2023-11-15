329726e2e824be2751740f548668a680607c2a3d
maryamosama33 <maryamosama3383@gmail.com>
2023-11-16 00:11:41 +0200

    Add Device class

--- RouterWiFiSimulator/src/Device.java
+++ RouterWiFiSimulator/src/Device.java
@@ -1,3 +1,34 @@
-public class Device {
-    
+class Device extends Thread {
+    private static int nextConnectionID = 1;
+    private final String deviceName;
+    private final Router router;
+    private int connectionID;
+
+    public Device(String name, Router router) {
+        this.deviceName = name;
+        this.router = router;
+        this.connectionID = -1; // No connection initially
+    }
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public int getConnectionID() {
+        return connectionID;
+    }
+
+    @Override
+    public void run() {
+        try {
+            router.connect(this);
+            router.performOnlineActivity(this);
+            router.disconnect(this);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    synchronized void assignConnectionID() {
+        connectionID = nextConnectionID++;
+    }
 }
